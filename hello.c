679673ac0104a8d014c432823386e68e27ca92d7
unknown <12348naveed@gmail.com>
2018-03-04 04:37:49 -0800

    a

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include "stdio.h"
-
+void microkernel_sendmsg(char *);
 int main(void) {
   printf("Hello World\n");
   return 0;
 }
+void microkernel_sendmsg(char *a){
+ printf("microkernel: %s\n", a);
+ }
