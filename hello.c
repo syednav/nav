bb73a3c7c5cc5ada68b27860e05df7c95a5e2dc1
unknown <12348naveed@gmail.com>
2018-03-04 05:59:43 -0800

    branch comment

--- hello.c
+++ hello.c
@@ -1,9 +1,17 @@
 #include "stdio.h"
 void microkernel_sendmsg(char *);
+void microkernel_getmsg(char *);
 int main(void) {
   printf("Hello World\n");
+printf("This must be a monolithic
+ design\n");
+microkernel_sendmsg("is more
+ portable");
   return 0;
 }
 void microkernel_sendmsg(char *a){
  printf("microkernel: %s\n", a);
  }
+void microkernel_getmsg(char *b){
+ //TODO: getmsg feature
+ }
